ee61407cb92358cd91fb8afae226cbe20bf3cdf9
XeroWTF <eugedsr@gmail.com>
2023-10-05 18:28:54 -0300

    fakedata

--- adopta-me/Client/src/Pages/Home.jsx
+++ adopta-me/Client/src/Pages/Home.jsx
@@ -2,9 +2,9 @@ import './Home.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useState, useEffect } from 'react';
 import PetCard from '../Components/PetCard';
-import AnimalForm from '../Components/AnimalForm';
 import LoginButton from '../Components/LoginButton'; 
 import LogoutButton from '../Components/LogoutButton';
+import fakePetData from "../Assets/fakePetData.json";
 
 const Home = () => {
   const [animals, setAnimals] = useState([]);
@@ -12,7 +12,8 @@ const Home = () => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      fetchAnimals();
+     // fetchAnimals();
+     setAnimals(fakePetData);
     }
   }, [isAuthenticated]);
 
@@ -39,14 +40,22 @@ const Home = () => {
 
       <main>
         {isAuthenticated ? (
-          <>
-            <PetCard animals={animals} />         
-          </>
+          <div className="pet-cards">
+            {animals.map((pet) => (
+              <PetCard
+                key={pet.id}
+                id={pet.id}
+                name={pet.name}
+                province={pet.province}
+                description={pet.description}
+                picture={pet.picture}
+              />
+            ))}
+          </div>
         ) : (
           <p>Necesitas iniciar sesión para ver esta sección</p>
         )}
       </main>
-
       <footer>
         <p>Copyright Adopta-me</p>
       </footer>
