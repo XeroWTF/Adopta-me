4d471dd20ab0c8314c2da83ace922c661903e83d
XeroWTF <eugedsr@gmail.com>
2023-10-09 20:46:05 -0300

    Agregando Pages

---
+++ adopta-me/Client/src/Pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileData from "../Components/ProfileData"
+
+const Profile = () => {
+
+    return (
+    <div className="addForm">
+        <ProfileData />
+    </div>
+    )
+}
+
+export default Profile;
